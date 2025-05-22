cf6439cbefaf8464c3d457c2b6f5a5334c7a333b
makabaka64 <2926978724@qq.com>
2025-05-22 20:04:44 +0800

    项目结构重整

--- react-vite-ts/src/App.tsx
+++ react-vite-ts/src/App.tsx
@@ -1,12 +1,9 @@
-import { MantineProvider } from '@mantine/core';
 import AppRouter from './router';
 import './index.scss';
 
 function App() {
   return (
-    <MantineProvider>
       <AppRouter />
-    </MantineProvider>
   );
 }
 
